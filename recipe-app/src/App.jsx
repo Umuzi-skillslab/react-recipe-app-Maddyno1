e3a6fc54d6801e74cbf06d1b3caa2ad1af1fdd6b
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-05 01:10:22 +0200

    Corrected import links

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from '../pages/Home'
-import RecipesPage from '../pages/RecipesPage'
-import RecipeDetail from '../pages/RecipeDetail'
-import MealPlannerPage from '../pages/MealPlannerPage'
-import FavoritesPage from '../pages/FavoritesPage'
-import NotFound from '../pages/NotFound'
-import Navbar from '../components/Navigation/Navbar'
+import Home from './pages/Home'
+import RecipesPage from './pages/RecipesPage'
+import RecipeDetail from './pages/RecipeDetail'
+import MealPlannerPage from './pages/MealPlannerPage'
+import FavoritesPage from './pages/FavoritesPage'
+import NotFound from './pages/NotFound'
+import Navbar from './components/Navigation/Navbar'
 
 function App() {
   return (
