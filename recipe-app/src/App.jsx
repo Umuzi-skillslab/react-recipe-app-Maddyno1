32edf84bf6eae0a9739f47c8068f5f65005967d8
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-04 14:37:08 +0200

    Minimal heading for each page file

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,4 +1,12 @@
 import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from '../pages/Home'
+import RecipesPage from '../pages/RecipesPage'
+import RecipeDetail from '../pages/RecipeDetail'
+import MealPlannerPage from '../pages/MealPlannerPage'
+import FavoritesPage from '../pages/FavoritesPage'
+import NotFound from '../pages/NotFound'
+import Navbar from '../components/Navigation/Navbar'
 
 
 export default App
---
+++ recipe-app/src/pages/MealPlannerPage.jsx
@@ -0,0 +1,5 @@
+function MealPlannerPage() {
+    return <h1>Meal Planner Page</h1>;
+}
+
+export default MealPlannerPage;
