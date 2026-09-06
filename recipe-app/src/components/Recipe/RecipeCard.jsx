fdd6c717f9367d7b630e50c5a579314dac9fcc17
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-06 18:44:23 +0200

    Added Recipe card with proptypes

---
+++ recipe-app/src/components/Recipe/RecipeCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+function RecipeCard({ recipe, isFavorite, onFavoriteToggle }) {
+    return (
+        <div className="recipe-card">
+            <img src={recipe.image} alt={recipe.title} />
+            <h3>{recipe.title}</h3>
+            <p>Cook time: {recipe.cookTime} minutes</p>
+            <Button
+                variant={isFavorite ? 'danger' : 'secondary'}
+                onClick={() => onFavoriteToggle(recipe.id)}
+            >
+                {isFavorite ? '❤️ Favorited' : '🤍 Favorite'}
+            </Button>
+        </div>
+    );
+}
+
+RecipeCard.PropTypes = {
+    recipe: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string,
+        cookTime: PropTypes.number,
+    }).isRequired,
+    isFavorite: PropTypes.bool,
+    onFavoriteToggle: PropTypes.func,
+};
+
+export default RecipeCard;
