9114fac38888a57c3d3267e0324a88445d5c013d
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-06 22:01:31 +0200

    Completed Recipes page

--- recipe-app/src/pages/RecipesPage.jsx
+++ recipe-app/src/pages/RecipesPage.jsx
@@ -1,5 +1,30 @@
-function RecipesPage() {
-    return <h1>Recipes Page</h1>;
+import { useState, useEffect } from 'react';
+import { recipesData } from '../data/recipesData';
+import SearchBar from '../components/UI/SearchBar';
+import RecipeList from '../components/Recipe/RecipeList';
+
+function RecipesPage({favorites, onFavoriteToggle }) {
+    const [recipes, setRecipes] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        setRecipes(recipesData);
+    }, []);
+
+    const filteredRecipes = recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+            <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+            <RecipeList
+                recipes={filteredRecipes}
+                favorites={favorites}
+                onFavoriteToggle={onFavoriteToggle}
+            />
+        </div>
+    );
 }
 
 export default RecipesPage;
