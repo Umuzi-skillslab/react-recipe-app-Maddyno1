ae2e11ded372cb567306e7642959353fa7c42947
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-07 20:07:10 +0200

    Added missing import and props

--- recipe-app/src/components/MealPlanner/MealPlanner.jsx
+++ recipe-app/src/components/MealPlanner/MealPlanner.jsx
@@ -1,16 +1,19 @@
-import DayCard from "./components/MealPlanner/DayCard";
+import DayCard from "./DayCard";
+import styles from "./MealPlanner.module.css";
 
 const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
-function MealPlanner({ mealPlan, onAddMeal, }) {
+function MealPlanner({ mealPlan, allRecipes,onAddMeal, onRemoveMeal }) {
     return (
-        <div className="meal-planner">
+        <div className={styles.planner}>
             {days.map(day => (
                 <DayCard
                     key={day}
                     day={day}
                     meals={mealPlan[day]}
+                    allRecipes={allRecipes}
                     onAddMeal={onAddMeal}
+                    onRemoveMeal={onRemoveMeal}
                 />
             ))}
         </div>
