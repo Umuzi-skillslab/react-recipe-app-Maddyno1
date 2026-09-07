449d4c047f7bbc0a013d90feb1452e3851d7419a
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-07 12:36:14 +0200

    Error

--- recipe-app/src/pages/RecipesPage.jsx
+++ recipe-app/src/pages/RecipesPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { recipesData } from '../data/recipesData';
 import SearchBar from '../components/UI/SearchBar';
 import RecipeList from '../components/Recipe/RecipeList';
-import Loading from '..components/UI/Loading';
+import Loading from '../components/UI/Loading';
 
 function RecipesPage({favorites, onFavoriteToggle }) {
     const [recipes, setRecipes] = useState([]);
