e5441fe5c211b67e9f4b08391ed9e0072238d5aa
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-07 17:08:54 +0200

    Added text for home page

--- recipe-app/src/pages/Home.jsx
+++ recipe-app/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import AudioPlayer from "../components/Media/AudioPlayer";
+
 function Home() {
-    return <h1>Home</h1>;
+    return (
+        <div>
+            <h1>Welcome to THE FLAMING INFERNO</h1>
+            <p>Browse a wide range of recipes from around the world, choose your favorites, and plan your meals for the week</p>
+            <p>FLAMING INFERNO: Scorching your taste buds with unbelievable flavor</p>
+            <AudioPlayer
+                audioUrl="https://www.w3schools.com/html/horse.mp3"
+                title="Cooking Tips: Getting started in the kitchen"
+            />
+        </div>
+    );
 }
 
 export default Home;
