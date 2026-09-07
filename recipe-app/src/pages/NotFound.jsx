dc66ab24ac6dc4e8ecf0e98984969ad4673fd30c
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-07 16:45:11 +0200

    Added notFound page

--- recipe-app/src/pages/NotFound.jsx
+++ recipe-app/src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
-function NotFound() {
-    return <h1>Not Found</h1>;
-}
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+    const navigate = useNavigate()
+
+    return(
+        <div className={styles.notFound}>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for doesn't exist.</p>
+            <button onClick={() => navigate('/')}>Go Home</button>
+        </div>
+    );
+};
 
 export default NotFound;
