a41328cbe6dbe06c9538699090746df15420d818
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-07 21:47:11 +0200

    Added proptypes to button component

--- recipe-app/src/components/UI/Button.jsx
+++ recipe-app/src/components/UI/Button.jsx
@@ -1,11 +1,20 @@
+import PropTypes from 'prop-types';
 import styles from './UI.module.css';
 
-function Button({ variant = 'primary', children, onClick }) {
+//Class is picked dynamically from the prop
+function Button({ variant = 'primary', type = "button", onClick, children }) {
     return (
-        <button className={`${styles.btn} ${styles[`btn-${variant}`]}`} onClick={onClick}>
+        <button type={type} className={`${styles.button} ${styles[variant]}`} onClick={onClick}>
             {children}
         </button>
     );
 }
+
+Button.prototype = {
+    variant: PropTypes.oneOf(["primary", "secondary", "danger"]),
+    type: PropTypes.string,
+    onClick: PropTypes.func,
+    children: PropTypes.node,
+};
 
 export default Button;
