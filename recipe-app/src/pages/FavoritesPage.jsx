a28369909ded483cb942b8adb701fbdcdd3a88a6
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-07 10:45:00 +0200

    Built favorites page

--- recipe-app/src/pages/FavoritesPage.jsx
+++ recipe-app/src/pages/FavoritesPage.jsx
@@ -1,5 +1,20 @@
-function FavoritesPage() {
-    return <h1>Favorites Page</h1>;
+import RecipeList from "../components/Recipe/RecipeList";
+
+function FavoritesPage({favorites, onFavoriteToggle }) {
+    return (
+        <div>
+            <h1>Favorites Page</h1>
+            {favorites.length > 0 ? (
+                <RecipeList
+                    recipes={favorites}
+                    favorites={favorites}
+                    onFavoriteToggle={onFavoriteToggle}
+                />
+            ) : (
+                <p>No favorites yet - start adding recipes!</p>
+            )}
+        </div>
+    );
 }
 
 export default FavoritesPage;
