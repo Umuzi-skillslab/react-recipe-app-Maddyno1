d4133716d51eff2835737d3e6ba48cc6b3183f19
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-08 14:58:41 +0200

    Wrapped audio in a card

--- recipe-app/src/pages/Home.jsx
+++ recipe-app/src/pages/Home.jsx
@@ -1,15 +1,19 @@
+import Card from "../components/Media/AudioPlayer";
 import AudioPlayer from "../components/Media/AudioPlayer";
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to THE FLAMING INFERNO</h1>
-            <p>Browse a wide range of recipes from around the world, choose your favorites, and plan your meals for the week</p>
-            <p>FLAMING INFERNO: Scorching your taste buds with unbelievable flavor</p>
-            <AudioPlayer
-                audioUrl="https://www.w3schools.com/html/horse.mp3"
-                title="Cooking Tips: Getting started in the kitchen"
-            />
+        <div className="page">
+            <h1>Welcome to THE FLAMING INFERNO 🔥</h1>
+            <p>Browse a wide range of recipes from around the world, save your favorites, and plan your meals for the week.</p>
+            <p><em>FLAMING INFERNO: Scorching your taste buds with unbelievable flavor.</em></p>
+            //Audio is wrapped in a card
+            <Card title="Cooking Tip of the Day!">
+                <AudioPlayer
+                    audioUrl="https://www.w3schools.com/html/horse.mp3"
+                    title="Cooking Tips: Getting started in the kitchen"
+                />
+            </Card>
         </div>
     );
 }
