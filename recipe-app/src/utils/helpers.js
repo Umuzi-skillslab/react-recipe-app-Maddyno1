df54998b91bbdb17334edbf2034810cd3e1a1bce
Matshidiso <matshidiso.moshoeshoe@umuzi.org>
2026-09-08 09:09:03 +0200

    Function that converts cookTime numbers for better UI

---
+++ recipe-app/src/utils/helpers.js
@@ -0,0 +1,7 @@
+//Converts numbers so that they look better for UI
+export function formatCooktime(minutes = 0) {
+    if (minutes < 60) return `${minutes} min`;
+    const hrs = Math.floor(minutes / 60);
+    const min = minutes % 60;
+    return mins ? `${hrs} hr ${mins} min` : `${hrs} hr`;
+}
